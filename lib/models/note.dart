9e768dd7e843992566f0572fe808403193c0e270
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2023-06-11 13:21:56 +0200

    update

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,17 +1,14 @@
 class Note {
-  int ?id;
   String? title;
   String? content;
 
   Note({
-    this.id,
     this.title,
     this.content,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'title': title,
       'content': content,
     };
@@ -19,7 +16,6 @@ class Note {
 
   static Note fromMap(Map<String, dynamic> map) {
     return Note(
-      id: map['id'],
       title: map['title'],
       content: map['content'],
     );
